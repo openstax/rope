97a88989c5b73398691a486a0b49dc0b66e9ecfb
Michael Reyna <MReyna12@live.com>
2024-02-01 11:42:12 -0600

    Add POST test

--- backend/tests/test_endpoints.py
+++ backend/tests/test_endpoints.py
@@ -2,12 +2,29 @@ from fastapi.testclient import TestClient
 from unittest.mock import patch
 import pytest
 from rope.api.main import app
-from rope.api.sessions import get_request_session
+from rope.api.sessions import get_request_session, sessions
+from rope.api.database import SessionLocal
+from rope.db.schema import UserAccount
 
 
 @pytest.fixture
 def test_client():
     return TestClient(app)
+
+
+@pytest.fixture
+def db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(autouse=True)
+def clear_database_table(db):
+    db.query(UserAccount).delete()
+    db.commit()
 
 
 def override_get_request_session():
@@ -61,3 +78,25 @@ def test_delete_session(test_client, mocker):
     )
     response = test_client.delete("/session")
     assert response.status_code == 200
+
+
+def test_google_login(test_client, db, mocker):
+    google_oauth_mock = mocker.Mock()
+    google_oauth_mock.verify_oauth2_token.return_value = {
+        "hd": "rice.edu",
+        "email": "test@rice.edu",
+    }
+    mocker.patch(
+        "rope.api.auth.id_token.verify_oauth2_token",
+        google_oauth_mock.verify_oauth2_token,
+    )
+    db_user = UserAccount(email="test@rice.edu", is_manager=False, is_admin=False)
+    db.add(db_user)
+    db.commit()
+    response = test_client.post("/session", json={"token": "fp2m3fpwimef"})
+    session_id = list(sessions.keys())[0]
+    user_data = sessions[session_id]
+    assert response.status_code == 200
+    assert user_data["email"] == "test@rice.edu"
+    assert user_data["is_manager"] is False
+    assert user_data["is_admin"] is False
