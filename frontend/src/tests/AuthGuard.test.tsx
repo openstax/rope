0e4483865d45282e52a7a5ace985cb98cfd87b86
Prabhdip Gill <pg53@rice.edu>
2024-02-14 11:34:45 -0800

    Mock location object to fix test warning

--- frontend/src/tests/AuthGuard.test.tsx
+++ frontend/src/tests/AuthGuard.test.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from '@testing-library/react'
 import { AuthContext, AuthStatus } from '../components/useAuthContext'
 import { AuthGuard } from '../components/AuthGuard'
-import { expect } from 'vitest'
+import { expect, vi } from 'vitest'
 import { PageContextProvider } from '../components/usePageContext'
 import { type PageContext } from 'vike/types'
 
 describe('AuthGuard', () => {
+  Object.defineProperty(window, 'location', {
+    value: {
+      href: './login'
+    }
+  })
+
   it('Authguard denies user protected content if user is not logged in', () => {
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
     const pageContext = {
@@ -21,7 +27,6 @@ describe('AuthGuard', () => {
       </AuthContext.Provider>
       </PageContextProvider>
     )
-
     const protectedContent = screen.queryByText('Protected content')
     expect(protectedContent).toBeNull()
   })
