c064a6f35d14847d83380864eac2ce1880c8ffb7
Michael Reyna <MReyna12@live.com>
2024-02-21 15:27:15 -0600

    Add base and full course build settings models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,6 @@
+from typing import Optional
+import enum
+
 from pydantic import BaseModel
 
 
@@ -30,4 +33,26 @@ class BaseMoodleSettings(BaseModel):
 
 
 class FullMoodleSettings(BaseMoodleSettings):
+    id: int
+
+
+class BaseCourseBuildSettings(BaseModel):
+    instructor_firstname: str
+    instructor_lastname: str
+    instructor_email: str
+    school_district: int
+
+
+class FullCourseBuildSettings(BaseCourseBuildSettings):
+    course_name: str
+    course_shortname: str
+    course_category: int
+    course_id: Optional[int] = None
+    course_enrollment_url: Optional[str] = None
+    course_enrollment_key: Optional[str] = None
+    academic_year: str
+    academic_year_short: str
+    base_course_id: int
+    creator: int
+    status: enum.Enum
     id: int
