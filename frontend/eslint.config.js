5e5b78b515d2b006f76dd309c1272b0edf682b1c
Michael Reyna <MReyna12@live.com>
2024-06-05 17:24:34 -0500

    Add additional configs and rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,17 +1,19 @@
-import react from "eslint-plugin-react"
+import react from "eslint-plugin-react/configs/recommended.js"
+import reactJsx from "eslint-plugin-react/configs/jsx-runtime.js"
 import hooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import love from "eslint-config-love"
 import tsParser from "@typescript-eslint/parser"
 import globals from "globals"
+import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js'
 
 export default [
   love,
-  {
-    rules: {
-      'react-refresh/only-export-components': 'warn',
-    }
-  },
+  react,
+  reactJsx,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: [
       '**/*.ts',
@@ -62,8 +64,12 @@ export default [
   {
     plugins: {
       'react-refresh': reactRefresh,
-      'react-hooks': hooks,
-      'react': react
+      'react-hooks': hooks
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn"
     }
   }
 ]
